033fa26bdc26a1588a2bbd675df22e637265b108
ypetler <yevhenii@webselenese.com>
2025-03-18 23:05:30 +0100

    kita app react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Assessment from './Assessment'; // Убедись, что путь правильный
+
+const App = () => {
+    return (
+        <div>
+            <Assessment />
+        </div>
+    );
+};
+
+export default App;
